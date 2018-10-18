0502c77fa12585081c13b1b3004ec33771142591
Mostafa AbdEl Fatah <mostafaabdelfatah@Mostafas-Mac.local>
2018-10-18 12:40:48 -0700

    first commit

--- SearchController/ViewController.swift
+++ SearchController/ViewController.swift
@@ -1,25 +1,53 @@
-//
-//  ViewController.swift
-//  SearchController
-//
-//  Created by Mostafa AbdEl Fatah on 10/18/18.
-//  Copyright © 2018 Mostafa AbdEl Fatah. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UIScrollViewDelegate ,UISearchResultsUpdating {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var filterArray:[String]?
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return (filterArray?.count)!
+        }else{
+            return Constants.countries.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if searchController.isActive && searchController.searchBar.text != "" {
+            cell.textLabel?.text = filterArray?[indexPath.row]
+        }else{
+            cell.textLabel?.text = Constants.countries[indexPath.row]
+        }
+        return cell
     }
 
-
+    func updateSearchResults(for searchController: UISearchController) {
+        self.filterArray = Constants.countries.filter(){
+            $0.range(of: searchController.searchBar.text!) != nil
+            //$0.contains(searchController.searchBar.text!)
+        }
+        self.tableView.reloadData()
+    }
+    
 }
 
+
+extension ViewController :UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    }
+}
